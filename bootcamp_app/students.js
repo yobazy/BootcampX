f80f70d16a53955e1502ee2f2e4cf2569cb2677a
Bazil <king.bazil.khan@gmail.com>
2022-06-06 02:24:09 +0000

    change queries to parametic style

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
-let cohort = process.argv[2];
-let limit = process.argv[3];
+let cohortName = process.argv[2];
+let limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,17 +11,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+pool.query(queryString, values)
+
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-})
+}).catch(err => console.error('query error', err.stack));
